62be4a4850ca3984f9429527c7817f181d19c14b
ammy <alenakyu@gmail.com>
2026-07-15 17:12:00 +0100

    day 1

---
+++ day-1/hello.py
@@ -0,0 +1,12 @@
+import math
+
+a = 2
+b = 3
+c = 10
+d = 8
+
+res1 = math.sqrt((a - b) ** 2)
+print(f"Euclidean distance between {a} and {b}: {res1}")
+
+res2 = math.sqrt((c - d) ** 2)
+print(f"Euclidean distance between {c} and {d}: {res2}")
